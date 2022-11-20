44d2fec95a637269ccbc10b9ab994b7fcb23f39f
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2022-11-20 12:38:46 +0100

    Añadido elementos a la DB
    DB populada
    
    Co-Authored-By: Pablo Álvarez <80222536+Pabalvcar@users.noreply.github.com>
    Co-Authored-By: Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
    Co-Authored-By: David Zamora <80247733+Dr-Vell@users.noreply.github.com>
    Co-Authored-By: Francisco J Migueles Student <99977786+Framigdom@users.noreply.github.com>
    Co-Authored-By: Francisco Javier Migueles Dominguez <80247805+xiscomigueles@users.noreply.github.com>

--- principal/models.py
+++ principal/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+from enum import Enum
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class ProductType(Enum):   # A subclass of Enum
+    FUND = "Funda"
+    CARG = "Cargadores"
+    PROT = "Protectores"
+    OTRO = "Otros"
+
+class ProductSections(Enum):   # A subclass of Enum
+    HUWI = "Huawei"
+    XIAO = "Xiaomi"
+    SAMG = "Samsung"
+    VIVO = "Vivo"
+    UNK = "Desconocido"
+
+
 class Producto(models.Model):
     
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
     imagen = models.URLField()
-    precio = models.IntegerField()
-    stock = models.IntegerField()
+    precio = models.DecimalField(decimal_places=2,max_digits=8,validators=[MinValueValidator(0)])
+    categoria = models.CharField(max_length=15,choices=[(tag.value,tag.value) for tag in ProductType],default=ProductType.OTRO)
+    secciones = models.CharField(max_length=15,choices=[(tag.value,tag.value) for tag in ProductSections],default=ProductSections.UNK)
+    stock = models.IntegerField(validators=[MinValueValidator(0)])
+
 
 
     def _str_(self):
