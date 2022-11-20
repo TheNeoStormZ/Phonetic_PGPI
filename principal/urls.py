f92859908bfde0f912a18a0bcd8176cb3fa2c9b7
Pablo Álvarez <100071510+PabloAlvarezCaroUS@users.noreply.github.com>
2022-11-20 20:52:18 +0100

    Añadida vista de producto
    Co-authored-by: David Zamora <Dr-Vell@users.noreply.github.com>
    Co-authored-by: Francisco J Migueles Student <Framigdom@users.noreply.github.com>
    Co-authored-by: Diego Ruiz Gil <DiegoRuizGil@users.noreply.github.com>

--- principal/urls.py
+++ principal/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('catalogo/', views.catalogo, name='catalogo'),
-    path('producto/', views.producto, name='producto'),
+    path('secciones/', views.secciones, name='secciones'),
+    path('producto/<int:producto_id>', views.producto, name='producto'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from principal.models import Producto
 from django.http import HttpResponse
 from django.template import loader
-import principal.models as models
+#import principal.models as models
  
 def index (request):
-  productos = models.Producto.objects.all().values()[:6]
+  productos = Producto.objects.all().values()[:6]
   template = loader.get_template('inicio.html')
   context = {
     'productos': productos
@@ -16,17 +13,59 @@ def index (request):
   
 def catalogo(request):
   template = loader.get_template('catalogo.html')
-  productos = models.Producto.objects.all().values()
-  lista_categorias = [categoria for categoria in models.Producto.objects.values_list('categoria', flat=True).distinct()]
+  productos = Producto.objects.all().values()
+  lista_categorias = [categoria for categoria in Producto.objects.values_list('categoria', flat=True).distinct()]
   context = {
     'productos':productos,
     'categorias':lista_categorias
   }
-  return HttpResponse(template.render(context, request))          
-
-
-def producto(request):
-  template = loader.get_template('producto.html')
+  return HttpResponse(template.render(context, request))       
+  
+def secciones(request):
+  template = loader.get_template('secciones.html')
+  productos = Producto.objects.all().values()
+  lista_secciones = [secciones for secciones in Producto.objects.values_list('secciones', flat=True).distinct()]
   context = {
+    'productos':productos,
+    'secciones':lista_secciones
+  }
+  return HttpResponse(template.render(context, request))      
+
+def producto(request, producto_id=''):
+  template = loader.get_template('producto.html')
+  producto = Producto.objects.filter(id=producto_id).values()
+  context = {
+    'producto':producto[0]
   }
   return HttpResponse(template.render(context, request))
+
+def busqueda(request):
+  template = loader.get_template('inicio.html')
+  q = request.GET.get('q', '')
+  if q:
+      qset = (
+        Producto(nombre__icontains=q)
+        )
+      results = Producto.objects.filter(qset).distinct()
+  else:
+        results = []
+  context = {
+    'productos':results
+  }
+  return HttpResponse(template.render(context, request))
+
+def search(request):
+  query = request.GET.get('q', '')
+  if query:
+      qset = (
+          Q(title__icontains=query) |
+          Q(authors__first_name__icontains=query) |
+          Q(authors__last_name__icontains=query)
+      )
+      results = Book.objects.filter(qset).distinct()
+  else:
+      results = []
+  return render_to_response("books/search.html", {
+      "results": results,
+      "query": query
+  })
