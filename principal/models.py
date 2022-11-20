b561cdfc8eda4e2c28cd0c2030a331b2fed3e58e
TheNeoStormZ <pablomarinpro@gmail.com>
2022-11-20 11:46:34 +0100

    Producto base realizado Se ha añadido un modelo basico de producto

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    imagen = models.URLField()
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+
+    def _str_(self):
+        return self.nombre
