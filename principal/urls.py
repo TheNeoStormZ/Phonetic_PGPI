eb6cf2b7ac78cdee4e244c5a5070e7a83387a062
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2022-11-26 19:22:41 +0100

    Versión inicial de cesta Funcionalidad basica
    Co-authored-by: David Zamora <Dr-Vell@users.noreply.github.com>

--- principal/urls.py
+++ principal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('secciones/<seccion_nombre>', views.secciones, name='secciones'),
     path('producto/<int:producto_id>', views.producto, name='producto'),
     path('buscar', views.buscar, name='search'),
+    path('cesta/<accion>/<int:cesta_item_id>', views.cesta, name='cesta'),
+    path('cesta/hide/<int:cesta_item_id>', views.cesta, name='cesta_hide'),
 ]
