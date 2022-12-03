46bb9956c951d971254eb2ca537dee3e735bae49
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2022-12-03 21:10:42 +0100

    Phonetic terminada Vamooooooooooooooo
    Co-authored-by: Francisco J Migueles Student <Framigdom@users.noreply.github.com>
    Co-authored-by: David Zamora <Dr-Vell@users.noreply.github.com>
    Co-authored-by: Pablo Álvarez <Pabalvcar@users.noreply.github.com>
    Co-authored-by: TheNeoStormZ <TheNeoStormZ@users.noreply.github.com>

--- principal/static/comprar.js
+++ principal/static/comprar.js
@@ -1,15 +1,21 @@
 
-async function comprarAsync (productoId) {
-    lote = document.getElementById('lote').value
+async function comprarAsync(productoId) {
+  var lote;
 
-    base_url =  window.location.protocol + String("//")+ window.location.hostname + String(":") + window.location.port
+  var element = document.getElementById('lote');
 
-    url = "/producto/" + String(productoId) + "?lote=" + String(lote) + "&&comprar"
-    
-    let response = await fetch(url);
-
-    window.location.href = "/checkout"
-    
-    return 0;
-
+  if (element != null) {
+    lote = element.value;
   }
+
+  base_url = window.location.protocol + String("//") + window.location.hostname + String(":") + window.location.port
+
+  url = "/producto/" + String(productoId) + "?lote=" + String(lote) + "&&comprar"
+
+  let response = await fetch(url);
+
+  window.location.href = "/checkout"
+
+  return 0;
+
+}
